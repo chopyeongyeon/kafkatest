1a57fdf1ab932dae9f3739e18e81184394078197
chopyeongyeon <vuddus526@epozen.com>
2025-09-02 10:02:36 +0900

    Initial commit for project

---
+++ src/test/java/com/epozen/kafkatest/KafkatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epozen.kafkatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
